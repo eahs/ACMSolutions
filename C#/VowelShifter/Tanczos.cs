4e90a57534aa0489f69731a1b9aab50b27c24a94
Michael Tanczos <mike@logic-gate.com>
2019-03-21 12:29:18 -0400

    Added VowelShifter

---
+++ C#/VowelShifter/Tanczos.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace VowelShifter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string original = "aeiouAEIOU";
+            string shifted = "eiouaEIOUA";
+
+            Console.WriteLine("Enter a sentence.");
+
+            string input = Console.ReadLine();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                int index = original.IndexOf(input[i]);
+
+                if (index != -1)
+                    Console.Write(shifted[index]);
+                else
+                    Console.Write(input[i]);
+            }
+
+
+        }
+    }
+}
