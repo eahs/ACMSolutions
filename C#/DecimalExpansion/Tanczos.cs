6c6c5e84d4c011a998db8e73e7fd170fbe765db7
Michael Tanczos <mike@logic-gate.com>
2019-03-25 14:00:35 -0400

    Added DecimalExpansion and LineOfSight

---
+++ C#/DecimalExpansion/Tanczos.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ACMHelper;
+
+namespace DecimalExpansion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> decimals = new List<int>();
+
+            Console.Write("Enter a positive integer: ");
+            int n = Console.ReadLine().ToInteger();
+            int remainder = 1, index;
+            bool canbreak = false;
+
+            while (true)
+            {
+                remainder *= 10;
+
+                if (remainder >= n) canbreak = true;
+
+                int digit = remainder / n;
+
+                index = decimals.IndexOf(digit);
+
+                if (index != -1 && canbreak) break;  // We are starting to repeat digits
+
+                decimals.Add(remainder / n);
+                remainder %= n;
+            }
+
+            string result = String.Join("", decimals);
+
+            if (index != -1)
+                result = result.Substring(0, index) + "[" + result.Substring(index) + "]";
+
+            Console.WriteLine("1/{0} = 0.{1}", n, result);
+        }
+    }
+}
---
+++ C#/LineOfSight/Tanczos.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using ACMHelper;
+
+namespace LineOfSight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a positive integer: ");
+            int n = Console.ReadLine().ToInteger();
+
+            List<string> points = new List<string>();
+            for (int r = 0; r <= n; r++)
+            {
+                for (int c = 0; c <= n; c++)
+                {
+                    if (r == c && c == 0) continue;
+
+                    int gcf = ACM.GreatestCommonDivisor(r, c);
+                    
+                    string point = (r/gcf) + "," + (c/gcf);   // Reduce every point by it's greatest common divisor (since that point was seen first)
+                    if (!points.Contains(point))
+                        points.Add(point);
+                }
+            }
+
+            Console.WriteLine("Points of L({0}) visible from the origin: {1}", n, points.Count);
+        }
+    }
+}
