7ad11263e4611ad9743d9ed53de4c7863edfd018
Michael Tanczos <mike@logic-gate.com>
2019-03-25 14:22:45 -0400

    Added C# WealthRedistribution

---
+++ C#/WealthRedistribution/Tanczos.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WealthRedistribution
+{
+    class Program
+    {
+        static int maxIndex (List<int> nums)
+        {
+            int index = nums.Count - 1;
+
+            for (int i = nums.Count-1; i >= 0; i--)
+            {
+                if (nums[i] > nums[index])
+                    index = i;
+            }
+            
+            return index;
+        }
+
+        static int minIndex(List<int> nums)
+        {
+            int index = 0;
+
+            for (int i = 0; i < nums.Count; i++)
+            {
+                if (nums[i] < nums[index])
+                    index = i;
+            }
+           
+            return index;
+        }
+
+        static void Main(string[] args)
+        {
+            int n = 15;
+            int count = 0;
+            bool redistribute = true;
+            List<int> nums = Enumerable.Range(n/2, n).ToList();
+
+            do
+            {
+                int min = minIndex(nums);
+                int max = maxIndex(nums);
+
+                redistribute = Math.Abs(nums[min] - nums[max]) > 1;
+
+                if (redistribute)
+                {
+                    count++;
+                    nums[min]++;
+                    nums[max]--;
+                }
+
+            } while (redistribute);
+
+            Console.WriteLine(count);
+        }
+    }
+}
