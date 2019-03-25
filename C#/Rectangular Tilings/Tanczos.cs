db4091b20161b792533c5553d1ab2e43ab0feae2
Michael Tanczos <mike@logic-gate.com>
2019-03-25 12:40:55 -0400

    Added rectangular tilings

---
+++ C#/Rectangular Tilings/Tanczos.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using ACMHelper;
+
+namespace RectangularTilings
+{
+    class Program
+    {
+        public static int sum (int n)
+        {
+            if (n == 1) return 1;
+
+            return n + sum(n - 1);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter width and height of grid: ");
+            List<int> nums = Console.ReadLine().ToIntegerList();
+
+            int soln = sum(nums[0]) * sum(nums[1]);
+            Console.WriteLine("{0} rectangular tilings.", soln);
+        }
+    }
+}
