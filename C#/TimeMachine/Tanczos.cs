6a7ea2c4572a00758a1701a0f7cf67091d30c45f
Michael Tanczos <mike@logic-gate.com>
2019-04-02 11:04:07 -0400

    Added C# Time Machine

---
+++ C#/TimeMachine/Tanczos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TimeMachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter time travel integer: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            DateTime dt = new DateTime(2014, 4, 22);
+            DateTime future = dt.AddDays(n);
+
+            Console.WriteLine("You will be transported to: {0}/{1}/{2}", future.Month, future.Day, future.Year);
+        }
+    }
+}
