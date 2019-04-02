669ce8ec61e67f3b8db1f746ba6812be378218e0
Michael Tanczos <mike@logic-gate.com>
2019-04-02 11:28:44 -0400

    Added C# DromedaryWords, HappyNumbers

---
+++ C#/DromedaryWords/Tanczos.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using ACMHelper;
+
+namespace DromedaryWords
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a word: ");
+            string word = Console.ReadLine();
+            List<int> vals = word.ToCharArray().Select(n => n - 'A').ToList();  // Turn into a list of integers
+
+            int hump = vals.IndexOf(vals.Max());  // Find the location of the hump
+            List<int> secondHalf = vals.GetRange(hump, vals.Count - hump);  // Get everything after the hump
+            secondHalf.Reverse();  // Reverse the list so we can just check if it too is IsIncreasing
+
+            if (vals.IsIncreasing(0, hump) && secondHalf.IsIncreasing())
+            {
+                Console.WriteLine("{0} is a dromedary word.", word);
+            }
+            else
+            {
+                Console.WriteLine("{0} is NOT a dromedary word.", word);
+            }
+        }
+    }
+}
---
+++ C#/HappyNumbers/Tanczos.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ACMHelper;
+
+namespace HappyNumbers
+{
+    class Program
+    {
+        public static bool isHappy(int n)
+        {
+            List<int> sums = new List<int>();
+            while (n > 1)
+            {
+                n = n.ToIntegerList().Select(k => k * k).Sum();
+
+                if (sums.Contains(n)) return false;
+                sums.Add(n);
+            }
+
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a positive integer: ");
+            int n = Console.ReadLine().ToInteger();
+
+            List<int> happyNumbers = Enumerable.Range(1, 1000000).Where(k => isHappy(k)).ToList();
+
+            int count = 0;
+            for (int i = 0; i < happyNumbers.Count-1; i++)
+            {
+                if (happyNumbers[i]+1 == happyNumbers[i+1])
+                {
+                    count++;
+
+                    if (count == n)
+                    {
+                        Console.WriteLine("Happy pair: {0}-{1}", happyNumbers[i], happyNumbers[i + 1]);
+                    }
+                }
+            }
+
+        }
+    }
+}
