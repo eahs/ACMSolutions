b61d8d547b9fb96c2326e58a5a5b6d00c4286184
Andrew Spaziani <spaziani96@gmail.com>
2019-04-02 10:16:05 -0400

    Added 2 C# files

---
+++ C#/ConsecutiveIntegerSum/Spaziani.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using ACMHelper;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace consecutiveIntegerSums
+{
+    class Spaziani
+    {
+
+        public static long Solve(int start, int end)
+        {
+            long Sol = 0;
+            for (int i = start; i <= end; i++)
+            {
+                Sol += i;
+            }
+
+            return Sol;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] Ans = new int[1000000];
+
+            for (int i = 1; i < Ans.Length/10; i++)
+            {
+                for (int j = i + 1; j < i + 100; j++)
+                {
+                    long a = Solve(i, j);
+
+                    if (a < Ans.Length)
+                        Ans[a]++;
+                }
+            }
+
+            while (true)
+            {
+                Console.Write("Enter a positive integer: ");
+                int input = Console.ReadLine().ToInteger();
+
+                List<int> answers = Ans.ToList();
+                Console.WriteLine("Output: {0}", answers.IndexOf(input));
+            }
+        }
+    }
+    
+}
---
+++ C#/Marbles/Spaziani.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using ACMHelper;
+using System.Collections.Generic;
+
+namespace Marbles
+{
+    class Spaziani
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Numerator and denominator: ");
+                List<int> Frac = Console.ReadLine().ToIntegerList();
+
+                double ans = Math.Round(((double)Frac[0] / (double)Frac[1]),10);
+
+                double i = 2;
+                double j = 1;
+                double A = 0;
+                while (A != ans)
+                {
+                    A = Math.Round((j / i) * ((j - 1) / (i - 1)),10);
+                    if (j >= i)
+                    {
+                        j = 1;
+                        i++;
+                    }
+                    j++;
+                }
+
+                Console.WriteLine("{0} black and {1} white", j-1, i - (j-1));
+            }
+        }
+    }
+}
